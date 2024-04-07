fcbc2d523736b96332cb81cf64a310cecabb93cb
emkr-13 <krisna.rata13@gmail.com>
2024-04-07 17:06:20 +0700

    update create topik

--- create_topik.py
+++ create_topik.py
@@ -8,7 +8,7 @@ bootstrap_servers = '10.11.13.81:9092'  # Ganti dengan alamat broker Kafka Anda
 admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
 
 # Tentukan nama topik baru dan konfigurasi jika diperlukan
-new_topic_name = 'onlinenewsres'
+new_topic_name = 'twitter_dki_backdate'
 new_topic = NewTopic(name=new_topic_name, num_partitions=1, replication_factor=1)
 
 # Buat topik baru menggunakan KafkaAdminClient
