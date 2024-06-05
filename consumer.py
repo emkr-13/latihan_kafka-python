af50cc74ee121be6cee4d9d4087691e9bdf2a92c
emkr-13 <krisna.rata13@gmail.com>
2024-06-05 22:07:44 +0700

    update malam ini

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer(
-    'twitter',
+    'twitter_24maret',
     bootstrap_servers='10.11.13.81:9092',
     auto_offset_reset='earliest'
 )
